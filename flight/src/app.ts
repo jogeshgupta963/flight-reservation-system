a25db9d3ea1abde6e4ccf621f2c931ed0f4b47eb
jogeshgupta963 <jogeshgupta963@gmail.com>
2023-07-06 15:33:09 +0530

    fix: added base route

--- flight/src/app.ts
+++ flight/src/app.ts
@@ -7,4 +7,7 @@ export const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
+app.get("/api/flight/index", (req, res) => {
+    res.send("Index Route for Flight service");
+});
 app.use("/api/flight", flightRouter);
